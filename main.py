390d343dd7a1e28bcd170aa053ca9bb802c41061
loki <kuznetsov@tochka.com>
2017-06-14 23:21:45 +0500

    done

--- main.py
+++ main.py
@@ -16,22 +16,56 @@ directories = {
 
 def people(docnum):
     for document in documents:
-        if docnum in document.get('number'):
-            print(document.get('number'))
+        if docnum == document.get('number'):
             print(document.get('name'))
 
 
 def list_doc(documents):
-    print(documents)
+    for doc in documents:
+        print(doc.get('type'), doc.get('number'), doc.get('name'))
 
 
 def shelf(docnum):
-    pass
+    for adr, docnumber in directories.items():
+        if docnum in docnumber:
+            print(adr)
 
 
-def add_doc(doc_property):
-    pass
+def add_doc():
+    doc_property = {}
+    typedoc = input('введите тип документа:')
+    numdoc = input('введите номер документа')
+    name = input('введите имя владельца')
+    doc_property.update({'type': typedoc, 'number': numdoc, 'name': name})
+    print('добавлен документ ', doc_property)
+    documents.append(doc_property)
+    dir_num = (input('Введите номер полки для хранения'))
+    if dir_num.isdigit():
+        if directories.get(dir_num):
+            directories.get(dir_num).append(numdoc)
+        else:
+            directories.update({dir_num: numdoc})
+    else:
+        'Номер должен быть числом'
+    print(directories.get(dir_num))
 
 
 if __name__ == '__main__':
-    people(str(input()))
+    while True:
+        choice = input('Какую операцию вы хотите выпполнить ? p : выведет имя по номеру документа ,\n'
+                       'l: выведет список всех документов\n'
+                       's: выведет номер полки по номеру документа\n'
+                       'a: добавит новый документ на выбраную полку\n'
+                       'наберите q для завершения работы \n')
+        if choice == 'p':
+            people(str(input('Введите номер документа :')))
+        elif choice == 'l':
+            list_doc(documents)
+        elif choice == 's':
+            shelf(str(input('Введите номер документа :')))
+        elif choice == 'a':
+            add_doc()
+        elif choice == 'q':
+            break
+        else:
+            print('Неизвестная команда попробуйте выбрать из списка')
